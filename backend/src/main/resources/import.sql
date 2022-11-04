f7ecdfa896a5f4d8f25799820415d17b9c539e92
vitor <vitorcc13@gmail.com>
2022-11-04 12:07:13 -0300

    Create database and seeders

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Maria Vitória', 222, 55, 3242.3, '2022-11-04');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Jeremias josé', 312, 12, 1142.8, '2022-11-04');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Carlos almeida', 123, 32, 3445.6, '2022-11-04');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('José selestiano', 43, 8, 6543.7, '2022-11-04');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Vandercleiton', 765, 43, 1123.2, '2022-11-04');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('José nhonho', 332, 54, 987.6, '2022-11-04');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Maria cicera', 234, 32, 1098.0, '2022-11-04');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Wilson limeira', 345, 22, 5098.1, '2022-11-04');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Odete lima', 100, 12, 444.54, '2022-11-04');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Eduardo da silva', 346, 12, 3465.0, '2022-11-04');
+
