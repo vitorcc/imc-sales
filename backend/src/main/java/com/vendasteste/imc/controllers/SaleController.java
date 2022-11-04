506fdc3819a9fe26d793cae53952f1d4fa9a0545
vitor <vitorcc13@gmail.com>
2022-11-04 18:57:57 -0300

    Twilio sms

--- backend/src/main/java/com/vendasteste/imc/controllers/SaleController.java
+++ backend/src/main/java/com/vendasteste/imc/controllers/SaleController.java
@@ -2,19 +2,20 @@ package com.vendasteste.imc.controllers;
 
 import com.vendasteste.imc.entities.Sale;
 import com.vendasteste.imc.services.SalesService;
+import com.vendasteste.imc.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
 
     @Autowired
     private SalesService salesService;
+    @Autowired
+    private SmsService smsService;
 
     @GetMapping
     public Page<Sale> findSales(
@@ -23,6 +24,10 @@ public class SaleController {
             Pageable pageable
     ){
         return salesService.findSales(minDate, maxDate, pageable);
+    }
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id){
+        smsService.sendSms(id);
     }
 
 }
